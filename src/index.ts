363c0e4821bff953866ee646fea3cf2a7da88f86
Lucky3028 <lucky3028.0@gmail.com>
2020-12-07 20:06:57 +0900

    [Remove] 不要なコメントアウト

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-// Modules to control application life and create native browser window
 import { app, BrowserWindow, Notification } from 'electron'
 import { ipcMain } from 'electron/main'
 import * as path from 'path'
 import { setTimeout } from 'timers'
 
 function createWindow() {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -15,22 +13,16 @@ function createWindow() {
     }
   })
 
-  // and load the index.html of the app.
   void mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
   mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 void app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+    // On macOS it's common to re-create a window in the app when the dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 
@@ -57,12 +49,6 @@ void app.whenReady().then(() => {
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
