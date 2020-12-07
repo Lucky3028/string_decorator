cc059b35c1be59a86b2b10905ace26d3c67ccc80
Lucky3028 <lucky3028.0@gmail.com>
2020-12-07 14:59:19 +0900

    [Fix] versionsのundefined回避

--- src/preload.ts
+++ src/preload.ts
@@ -1,13 +1,3 @@
-// TODO 別ファイルへ
-// https://dev.classmethod.jp/articles/assertion-functions-of-typescript-3-7/
-function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
-  if (val === undefined || val === null) {
-    throw new Error(
-      `Expected 'val' to be defined, but received ${val}`
-    );
-  }
-}
-
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
@@ -18,7 +8,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
   for (const type of ['chrome', 'node', 'electron']) {
     const versions = process.versions[type]
-    assertIsDefined(versions)
-    replaceText(`${type}-version`, versions)
+    if (typeof versions !== 'undefined') {
+      replaceText(`${type}-version`, versions)
+    }
   }
 })
