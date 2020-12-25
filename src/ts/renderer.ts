729d49503cb030c481ce8b4397fb24bafb0ae14d
Lucky3028 <lucky3028.0@gmail.com>
2020-12-25 21:45:27 +0900

    [Change] Bulmaを使うように

--- src/ts/renderer.ts
+++ src/ts/renderer.ts
@@ -7,20 +7,25 @@ tabLinks.forEach((clickedLabel) => {
     e.preventDefault()
 
     // activeであることを示すクラスタグの定義
-    const activeClassName = 'is_active'
+    const activeClassName = 'is-active'
+    // hiddenであることを示すクラスタグの定義
+    const hiddenClassName = 'is-hidden'
 
-    // タブとページコンテンツのis_activeを消す
+    // タブのis-activeを消す
     Array.from(tabLinks).forEach((link) => link.classList.remove(activeClassName))
-    Array.from(tabContents).forEach((cont) => cont.classList.remove(activeClassName))
 
-    // クリックされたタブとそれに対応するページコンテンツにis_activeをつける
+    // ページコンテンツに全てis-hiddenをつける
+    Array.from(tabContents).forEach((cont) => cont.classList.add(hiddenClassName))
+
+    // クリックされたタブにis-activeをつける
     clickedLabel.classList.add(activeClassName)
 
+    // 対応するページコンテンツからis-hiddenを消去する
     const dataId = clickedLabel instanceof HTMLElement ? clickedLabel.dataset.id : undefined
     if (dataId == undefined) return
 
     Array.from(tabContents)
       .filter((cont) => cont instanceof HTMLElement && dataId == cont.dataset.id)
-      .forEach((cont) => cont.classList.add(activeClassName))
+      .forEach((cont) => cont.classList.remove(hiddenClassName))
   })
 })
